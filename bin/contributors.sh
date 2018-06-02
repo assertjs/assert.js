048b49db598ea0590baa4ea03222632ddcfe236e
Mtronix <mike@mtronix.pl>
2018-06-02 21:50:17 +0200

    Add TravisCI configuration and Makefile

--- bin/contributors.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Script for generating CONTRIBUTORS file
-
-# $1 -> https://api.github.com/repos/assertjs/assert.js/contributors
-# $2 -> filepath
-
-curl -s $1 | jq -r '.[] | .login + " (" + .html_url + ")"' > $2
